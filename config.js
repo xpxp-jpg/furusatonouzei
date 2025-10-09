a1e557ab860a8b4e0a3154b17784439cd553a9b6
xpxp-jpg <gwycoco@gmail.com>
2025-10-09 09:48:56 +0900

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // AdSense/GA settings (edit safely)
 window.AD_CLIENT = "ca-pub-5317898094646973";   // your AdSense publisher ID
-window.AD_SLOT_ID = "";                         // <-- TODO: set your display ad Unit's data-ad-slot (string). Keep empty to disable manual ad.
+window.AD_SLOT_ID = "7284699546";                         // <-- TODO: set your display ad Unit's data-ad-slot (string). Keep empty to disable manual ad.
 window.GA4_ID    = "G-YS6NM3C8KG";             // GA4
